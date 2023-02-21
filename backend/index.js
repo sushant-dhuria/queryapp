053fd00f8ae47ddc8b063900df481205cb0d3aca
sushant-dhuria <75539938+sushant-dhuria@users.noreply.github.com>
2023-02-21 19:27:24 +0530

    done

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 8080 
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
+const mongoose = require('mongoose')
+mongoose.connect("mongodb+srv://sushant:sushant123@cluster0.q3jlgms.mongodb.net/?retryWrites=true&w=majority",{ useNewUrlParser: true, useUnifiedTopology: true })
+
+
+app.use(cors())
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+app.use('/',require('./routes/user.route'))
+app.listen(port,()=>{
+    console.log('port running on '+port)
+})
